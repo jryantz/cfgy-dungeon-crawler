c86f3dc32d224fb8ba3b520fe4e25e5293095533
Julian castillo <jgcastillo0@frostburg.edu>
2015-04-29 19:26:31 -0400

    changed name to lvl
    lvl

--- classes/levels/map.js
+++ classes/levels/map.js
@@ -1,7 +1,7 @@
 //map index 0 = 1st column, 1 = 2nd column, 2 = 3rd column, 3 = 4th column, 4 = 5th column
 // = each column = 5x[ability to enter,is an exit, enemy in cell,item in cell, array of enemy and item ids?]
 var currentLvl,
-    map = [[[0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []]],
+    lvl = [[[0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []]],
            [[0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []]],
            [[0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []]],
            [[0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []]],
@@ -9,8 +9,8 @@ var currentLvl,
            
 
  //sets everything 0 and removes items/enemies from array in each cell
-function resetMap() {
-	map = [[[0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []]],
+function resetlvl() {
+	lvl = [[[0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []]],
            [[0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []]],
            [[0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []]],
            [[0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []], [0, 0, 0, 0, []]],
@@ -22,18 +22,18 @@ function createLvl1() {
     resetMap();
     currentLvl = 1;
     //  first array collumn 0-4, second array row 0-4,third array (look at line 2 for what each index is)
-    map[0][3][0] = 1;
-    map[0][4][0] = 1;
-    map[1][3][0] = 1;
-    map[2][1][0] = 1;
-    map[2][1][1] = 1;
-    map[2][3][0] = 1;
-    map[2][4][0] = 1;
-    map[2][4][1] = 1;
-    map[3][1][0] = 1;
-    map[3][2][0] = 1;
-    map[3][3][0] = 1;
-    map[4][2][0] = 1;
+    lvl[0][3][0] = 1;
+    lvl[0][4][0] = 1;
+    lvl[1][3][0] = 1;
+    lvl[2][1][0] = 1;
+    lvl[2][1][1] = 1;
+    lvl[2][3][0] = 1;
+    lvl[2][4][0] = 1;
+    lvl[2][4][1] = 1;
+    lvl[3][1][0] = 1;
+    lvl[3][2][0] = 1;
+    lvl[3][3][0] = 1;
+    lvl[4][2][0] = 1;
 	placeEntities();
 }
 
@@ -45,30 +45,30 @@ function placeEntities() {
         //second for loop goes through the rows
         for (row < 5, row++){
             //checks if the current cell is accessable to player
-            if(map[column][row][0] == 1){
+            if(lvl[column][row][0] == 1){
                 //generates if there is an enemy
-                map[column][row][2] = Math.random() * 1;
+                lvl[column][row][2] = Math.random() * 1;
                 //generates if there is an item
-                map[column][row][3] = Math.random() * 1;
+                lvl[column][row][3] = Math.random() * 1;
                 
             //checks if enemy in cell and adds to item/enemy array
-            if(map[column][row][2] = 1)
+            if(lvl[column][row][2] = 1)
             {
-                map[column][row][5][0] = currentLvl;
+                lvl[column][row][5][0] = currentLvl;
             }
             
             else{
-             map[column][row][5][0] = 0;  
+             lvl[column][row][5][0] = 0;  
             }
             
             //checks if item in cell and adds to item/enemy array
-            if(map[column][row][3] = 1)
+            if(lvl[column][row][3] = 1)
             {
-                map[column][row][5][1] = currentLvl;
+                lvl[column][row][5][1] = currentLvl;
             }
             
             else{
-             map[column][row][5][1] = 0;  
+             lvl[column][row][5][1] = 0;  
             }
             
             }
