ecde68c4a0e155ccbb88e06e2962ec5db22bb2dd
Jonathan Yantz <ijryantz@gmail.com>
2015-04-29 20:35:28 -0400

    fixed spacing

--- classes/levels/Map.js
+++ classes/levels/Map.js
@@ -20,7 +20,7 @@ function resetlvl() {
 function createLvl1() {
     resetlvl();
     currentLvl = 1;
-    //  first array collumn 0-4, second array row 0-4,third array (look at line 2 for what each index is)
+    //first array collumn 0-4, second array row 0-4,third array (look at line 2 for what each index is)
     lvl[3][0][0] = 1;
     lvl[4][0][0] = 1;
     lvl[3][1][0] = 1;
