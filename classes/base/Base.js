001fadb9c76e01d437051607532a51f58203f967
Jonathan Yantz <ijryantz@gmail.com>
2015-04-29 22:21:53 -0400

    fix base, fix enemy, update and fix item

--- classes/base/Base.js
+++ classes/base/Base.js
@@ -1,15 +1,13 @@
-var baseLvl = 0;
-var baseExt1 = false;
-
+var baseLvl = 0,
+    baseExt1 = false;
 
 function upgradeBase() {
     if (plaMoney > = 200){
         baseLvl = 1;
         baseExt1 = true;
-        return "Congratulations, your base has recieved a slot where a merchant can reside";
-    }
-    else{
-        return "Come back when you have sufficient coin";
+        return "Congratulations, your base has recieved a new slot where a merchant can reside.";
+    } else {
+        return "Come back when you have sufficient coins.";
     }
 }
 
