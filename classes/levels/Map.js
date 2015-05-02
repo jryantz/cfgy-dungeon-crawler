24ca8f1c06e8f79f26bd93ed3383ff5a07ff770d
Julian castillo <jgcastillo0@frostburg.edu>
2015-05-01 22:11:08 -0400

    added function to check the exits
    checks the exits to find out if it goes to the next level or the
    previous level, commented them out will add them in when i look over
    what they will interact with

--- classes/levels/Map.js
+++ classes/levels/Map.js
@@ -1,6 +1,8 @@
 //map index 0 = 1st column, 1 = 2nd column, 2 = 3rd column, 3 = 4th column, 4 = 5th column
-// = each column = 5x[ability to enter,is an exit, enemy in cell,item in cell, array of enemy and item ids?]
+// = each column = 5x[ability to enter,is an exit(1 to previous,2 to nextlvl), enemy in cell,item in cell, array of enemy and item ids?]
 var currentLvl = 1,
+	nextLvl = currentLvl + 1;
+	prevLvl = currentLvl - 1;
     lvl = [[[0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]]],
 		   [[0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]]],
 		   [[0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]]],
@@ -16,9 +18,48 @@ function resetlvl() {
 		   [[0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]], [0, 0, 0, 0, [null, null, null]]]];
 }
 
+//checks to see if the cell has an exit and whether the exit goes to the next or previous levels
+/*function checkExit(PlaCurrCell)
+{
+var x = plaCurrCell [0],
+	y = plaCurrCell [1]
+	if(lvl[x][y][1] >0)
+	{
+		if(lvl[x][y][1] == 1)
+		{
+		makeLvl(prevLvl);
+		}
+		
+		else if (lvl[x][y][1] == 2)
+		{
+		makeLvl(nextLvl);
+		}
+	}
+
+}
+
+checks which level the map is creating
+function makeLvl(whichLvl)
+{
+	switch(whichLvl)
+	{
+	case 5: createLvl5();
+			break;
+	case 4: createLvl4();
+			break;
+	case 3: createLvl3();
+			break;
+	case 2: createLvl2();
+			break;
+	case 1: createLvl1();
+			break;
+	default: break;
+	
+	}
+}
+*/
 //creates level1
 function createLvl1() {
-    currentLvl = 1;
     //first array collumn 0-4, second array row 0-4,third array (look at line 2 for what each index is)
     lvl[3][0][0] = 1;
     lvl[4][0][0] = 1;
