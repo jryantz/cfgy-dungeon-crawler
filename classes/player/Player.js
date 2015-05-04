1c90e14a0f9672fd0b2f702da62d9d5be6ed0f19
Patrick Geiger <patrickjgeiger@gmail.com>
2015-05-04 13:45:17 -0400

    Changing Player Class
    Added in a way to ensure the player's stats do not go below 8 for
    character generation. It will continue to reroll the stat until it is
    above 8. Intelligence needs to be changed in any instance that it is
    used to Vitality. I did not change it on the player class yet as I did
    not want to break half the program. I can change it myself, but if
    someone wants to just go through and do that they're more than welcome.

--- classes/player/Player.js
+++ classes/player/Player.js
@@ -4,7 +4,7 @@ var plaLvl = 0,
     plaMaxHealth = 0,
     plaHealth = 0,
     plaStr = 0,
-    plaInt = 0,
+    plaInt = 0, //We scrapped the skill system therefore intelligence is no longer needed. This needs to be changed to Vitality. If I don't end up going through everything to change the variables then it does at some point need to be changed.
     plaAgi = 0,
     plaMoney = 0,
     
@@ -20,9 +20,30 @@ var plaLvl = 0,
     plaBg = [];
 
 function plaConstruct() {
-    plaStr = Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1);
-    plaInt = Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1);
-    plaAgi = Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1);
+    //the following while + if statements will ensure that the player's stats, at creation, will not dip below 8. This is in the favor of the player, of course, but the negatives you'd get from having a strength of 3, for       example, would literally be crippling to the point where you might as well refresh the page.
+    var str = 0,
+        agi = 0,
+        int = 0,
+        checker = false;
+    while (checker === false) {
+        str = Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1);
+        if (str > 8) {
+            plaStr = str;
+            checker = true;
+        }
+    checker = false;
+    while (checker === false) {
+        int = Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1);
+            plaInt = int;
+            checker = true;
+        }
+    checker = false; 
+    while (checker === false) {
+        agi = Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1) + Math.floor((Math.random() * 6) + 1);
+        if (agi > 8) {
+            plaAgi = agi;
+            checker = true;
+        }
     
     plaLvl = 1;
     
@@ -33,7 +54,7 @@ function plaConstruct() {
 }
 
 function plaLvlUp() {
-    if(plaXp >= plaMaxXp) {
+    if (plaXp >= plaMaxXp) {
         plaXp = plaXp - plaMaxXp;
         plaMaxXp += Math.floor(plaMaxXp * 0.75);
         plaLvl += 1;
